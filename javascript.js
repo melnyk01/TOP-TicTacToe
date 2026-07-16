ccc0eb243145a3c36bec12ef0a1b5747626ae6b8
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-16 20:41:22 +0200

    Add logic to play round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let gameBoard = {
-    board =[
+    board: [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
@@ -12,7 +12,39 @@ let game = {};
 function createPlayer(name, marker) {
     this.name = name;
     this.marker = marker;
-    sayHi = () => console.log(`Hi, my name is ${this.name}`);
+    this.sayHi = () => console.log(`Hi, my name is ${this.name}`);
 };
 
 const alex = new createPlayer('alex', 'O');
+const john = new createPlayer('john', 'x');
+john.sayHi()
+alex.sayHi()
+
+function playRound() {
+    for (row of gameBoard.board) {
+        console.log(row);
+    }
+    const humanChoice = +prompt('Please make a move by entering the number of square on a board');
+    let humanChoiceRow = Math.floor(humanChoice / 3);
+    let humanChoiceColumn = (humanChoice % 3) - 1;
+    if (humanChoice % 3 == 0) {
+        humanChoiceRow = (humanChoice / 3) - 1;
+        humanChoiceColumn = 2;
+    }
+
+    const computerChoice = +'9';
+    let computerChoiceRow = Math.floor(computerChoice / 3);
+    let computerChoiceColumn = (computerChoice % 3) - 1;
+    if (computerChoice % 3 == 0) {
+        computerChoiceRow = (computerChoice / 3) - 1;
+        computerChoiceColumn = 2;
+    }
+
+    gameBoard.board[humanChoiceRow][humanChoiceColumn] = 'O';
+    gameBoard.board[computerChoiceRow][computerChoiceColumn] = 'X';
+    for (row of gameBoard.board) {
+        console.log(row);
+    }
+}
+
+playRound();
