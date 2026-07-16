f0a1253d6006f6a9577f2d143b238ca42eb7b23f
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-16 23:17:12 +0200

    Optimized round logic

--- javascript.js
+++ javascript.js
@@ -3,48 +3,97 @@ let gameBoard = {
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0]
-    ]
+    ],
+    takenSquares: [],
 };
 
 let players = {};
-let game = {};
+let game = {
+    alexScore: 0,
+    johnScore: 0,
+    tie: 0,
+};
+
 
 function createPlayer(name, marker) {
     this.name = name;
     this.marker = marker;
-    this.sayHi = () => console.log(`Hi, my name is ${this.name}`);
 };
 
+function getHumanChoice() {
+    let humanChoice = +prompt('Please make a move by entering the number of square on a board');
+    while (gameBoard.takenSquares.includes(humanChoice)) {
+        humanChoice = +prompt('This square is already taken, please choose another one');
+    }
+    gameBoard.takenSquares.push(humanChoice);
+    humanChoice = getPosition(humanChoice);
+    return humanChoice;
+}
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * (9) + 1);
+    while (gameBoard.takenSquares.includes(computerChoice)) {
+        computerChoice = Math.floor(Math.random() * (9) + 1);
+    }
+    gameBoard.takenSquares.push(computerChoice);
+    computerChoice = getPosition(computerChoice);
+    return computerChoice;
+
+}
+function getPosition(square) {
+    let row = Math.floor(square / 3);
+    let column = (square % 3) - 1;
+    if (square % 3 == 0) {
+        row = (square / 3) - 1;
+        column = 2;
+    }
+    return [row, column]
+}
 const alex = new createPlayer('alex', 'O');
 const john = new createPlayer('john', 'x');
-john.sayHi()
-alex.sayHi()
 
-function playRound() {
-    for (row of gameBoard.board) {
-        console.log(row);
-    }
-    const humanChoice = +prompt('Please make a move by entering the number of square on a board');
-    let humanChoiceRow = Math.floor(humanChoice / 3);
-    let humanChoiceColumn = (humanChoice % 3) - 1;
-    if (humanChoice % 3 == 0) {
-        humanChoiceRow = (humanChoice / 3) - 1;
-        humanChoiceColumn = 2;
+
+function playGame() {
+
+    function playRound() {
+        let roundFlag = true;
+        while (roundFlag) {
+            let oMarkerNumber = 0;
+            let xMarkerNumber = 0;
+
+
+            if (roundFlag) {
+                humanChoice = getHumanChoice();
+                computerChoice = getComputerChoice();
+
+                gameBoard.board[humanChoice[0]][humanChoice[1]] = 'O';
+                gameBoard.board[computerChoice[0]][computerChoice[1]] = 'X';
+
+
+            }
+            for (row of gameBoard.board) {
+                oMarkerNumber = 0;
+                xMarkerNumber = 0;
+                for (column of row) {
+                    if (column == 'O') {
+                        oMarkerNumber++;
+                    } else if (column == 'X') {
+                        xMarkerNumber++;
+                    }
+                }
+                console.log(row);
+                if (oMarkerNumber == 3 || xMarkerNumber == 3) {
+                    roundFlag = false;
+                }
+
+            }
+
+        }
+
     }
 
-    const computerChoice = +'9';
-    let computerChoiceRow = Math.floor(computerChoice / 3);
-    let computerChoiceColumn = (computerChoice % 3) - 1;
-    if (computerChoice % 3 == 0) {
-        computerChoiceRow = (computerChoice / 3) - 1;
-        computerChoiceColumn = 2;
-    }
+    playRound();
 
-    gameBoard.board[humanChoiceRow][humanChoiceColumn] = 'O';
-    gameBoard.board[computerChoiceRow][computerChoiceColumn] = 'X';
-    for (row of gameBoard.board) {
-        console.log(row);
-    }
 }
 
-playRound();
+
+playGame();
