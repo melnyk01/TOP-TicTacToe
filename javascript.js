677eb565c8a9a9b1863750c5897d874f06039a1c
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-07-17 23:44:11 +0200

    Refractor game logic and separate UI handling

--- javascript.js
+++ javascript.js
@@ -1,45 +1,61 @@
 let gameBoard = {
     board: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ],
     takenSquares: [],
 };
-
-
-
-
-
-
-
-uiBoard = {};
 const board = document.querySelector('.board');
 
-for (row of gameBoard.board) {
-    for (column of row) {
-        const button = document.createElement('button');
-        button.innerText = column;
-        board.appendChild(button)
+
+dom = {
+    renderBoard() {
+
+        let buttonId = 1;
+        for (row of gameBoard.board) {
+            for (column of row) {
+                const button = document.createElement('button');
+                button.innerText = column;
+                button.id = buttonId;
+                board.appendChild(button);
+                buttonId++;
+            }
+
+        }
+        board.addEventListener('click', (event) => {
+            game.playerChoice = event.target
+            if (game.playerChoice.tagName == 'BUTTON' && game.playerChoice.className != 'taken' && game.isActive == true) {
+                game.playerChoice.textContent = 'O';
+                game.playerChoice.className = 'taken';
+                gameBoard.takenSquares.push(+game.playerChoice.id);
+                computerMove();
+                let cell = getPosition(game.playerChoice.id);
+                console.log(cell);
+                gameBoard.board[cell[0]][cell[1]] = 'O';
+                console.log(game.computerChoice);
+                cell = getPosition(game.computerChoice);
+                console.log(cell);
+                gameBoard.board[cell[0]][cell[1]] = 'X';
+                winnerCheck();
+
+            }
+        })
+
+    },
+
+    updateCell(cell) {
+        const square = document.getElementById(cell);
+        square.textContent = 'X'
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
 
 let game = {
     tie: 0,
     isActive: false,
     roundWinner: '',
+    playerChoice: '',
+    computerChoice: '',
 };
 let players = [];
 
@@ -54,26 +70,17 @@ player = new createPlayer('player', 'O');
 players.push(player)
 computer = new createPlayer('computer', 'X');
 players.push(computer);
-console.log({ game, players });
 
-function getHumanChoice() {
-    let humanChoice = +prompt('Please make a move by entering the number of square on a board');
-    while (gameBoard.takenSquares.includes(humanChoice)) {
-        humanChoice = +prompt('This square is already taken, please choose another one');
-    }
-    gameBoard.takenSquares.push(humanChoice);
-    humanChoice = getPosition(humanChoice);
-    return humanChoice;
-}
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * (9) + 1);
-    while (gameBoard.takenSquares.includes(computerChoice)) {
-        computerChoice = Math.floor(Math.random() * (9) + 1);
+    game.computerChoice = Math.floor(Math.random() * (9) + 1);
+    while (gameBoard.takenSquares.includes(game.computerChoice)) {
+        game.computerChoice = Math.floor(Math.random() * (9) + 1);
     }
-
-    gameBoard.takenSquares.push(computerChoice);
-    computerChoice = getPosition(computerChoice);
-    return computerChoice;
+    gameBoard.takenSquares.push(game.computerChoice);
+    const button = document.getElementById(game.computerChoice);
+    button.className = 'taken';
+    return game.computerChoice;
 
 }
 function getPosition(square) {
@@ -136,7 +143,6 @@ function diagonalCheck() {
     let oMarkerNumber = 0;
     let xMarkerNumber = 0;
     for (let i = 0; i < 3; i++) {
-        console.log(i);
         if (gameBoard.board[i][i] == 'O') {
             oMarkerNumber++;
         } else if (gameBoard.board[i][i] == 'X') {
@@ -178,11 +184,9 @@ function displayRoundResult() {
     } else if (game.roundWinner == computer.name) {
         computer.score++
     }
-    console.log(`${game.roundWinner} won!`);
-    console.log(`Score: ${player.name}: ${player.score} | ${computer.name}: ${computer.score} | Tie: ${game.tie}`)
-}
-function printBoard() {
-    for (row of gameBoard.board) console.log(row);
+    dialog.setAttribute('open', '');
+    winner.textContent = (`${game.roundWinner} won!`);
+    score.textContent = (`Score: ${player.name}: ${player.score} | ${computer.name}: ${computer.score} | Tie: ${game.tie}`);
 }
 
 function winnerCheck() {
@@ -192,31 +196,48 @@ function winnerCheck() {
     } else if (gameBoard.takenSquares.length > 8) {
         game.isActive = false;
         game.tie++
-        console.log('Looks like it\'s a tie!');
-        console.log(`Score: ${player.name}: ${player.score} | ${computer.name}: ${computer.score} | Tie: ${game.tie}`)
+        dialog.setAttribute('open', '');
+        winner.textContent = ('Looks like it\'s a tie!');
+        score.textContent = (`Score: ${player.name}: ${player.score} | ${computer.name}: ${computer.score} | Tie: ${game.tie}`)
     }
 }
 
+function computerMove() {
+    if (gameBoard.takenSquares.length < 9) {
+        game.computerChoice = getComputerChoice();
+        dom.updateCell(game.computerChoice);
+    }
+}
+
+function clearBoard() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            gameBoard.board[i][j] = '';
+        }
+    }
+    while (board.lastChild) {
+        board.removeChild(board.lastChild)
+    }
+    gameBoard.takenSquares = [];
+}
+
+const dialog = document.querySelector('.winner');
+const winner = document.querySelector('#winner');
+const score = document.querySelector('#score');
+const dialogBtn = document.querySelector('#dialog');
+dialogBtn.addEventListener('click', () => {
+    clearBoard();
+    playRound();
+})
+
 function playRound() {
+    dom.renderBoard();
     game.roundWinner = '';
     game.isActive = true;
-    while (game.isActive) {
-        humanChoice = getHumanChoice();
-        if (gameBoard.takenSquares.length < 9) {
-            computerChoice = getComputerChoice();
-        }
-        gameBoard.board[humanChoice[0]][humanChoice[1]] = 'O';
-        gameBoard.board[computerChoice[0]][computerChoice[1]] = 'X';
-        printBoard();
-
-        winnerCheck();
-    }
 }
 
 function playGame() {
-
     playRound();
-
 }
 playGame();
 
